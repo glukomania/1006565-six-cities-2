18f89b3db247dcf8eb414b09d8370ae94a2420fd
glukomania <glukomani@gmail.com>
2019-10-18 11:35:05 +0200

    Props have been added

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,16 @@
+import PropTypes from 'prop-types';
 import MainScreen from '../main-screen/main-screen';
 
-const App = () => {
-  return <MainScreen />;
+const App = (props) => {
+  const {places} = props;
+
+  return <MainScreen
+    places={places}
+  />;
+};
+
+App.propTypes = {
+  places: PropTypes.array.isRequired
 };
 
 export default App;
