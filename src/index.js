f4d555b7a3d55fc6731b0bdbd3ae4e0554c21e43
glukomania <glukomani@gmail.com>
2019-10-21 12:49:37 +0200

    mocks are edited a little

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './components/app/app';
 
 const init = () => {
   const places = [
-    {name: `Beautiful & luxurious apartment at great location`},
+    {name: `Beautiful & luxurious apartment at great location`, image: ``, price: ``, rating: ``, type: ``},
     {name: `Wood and stone place`},
     {name: `Canal View Prinsengracht`},
     {name: `Nice, cozy, warm big bed apartment`},
