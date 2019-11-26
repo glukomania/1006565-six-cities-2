f8fa5c4f8a25aeb28a6e519bba460d66fe03219f
glukomania <glukomani@gmail.com>
2019-11-26 14:56:48 +0100

    Comment ha been added

--- src/components/offer/offer.jsx
+++ src/components/offer/offer.jsx
@@ -8,6 +8,7 @@ import Feedback from './components/feedback/feedback';
 import {Operations} from '../../store/reducer';
 import Map from '../../components/map/map';
 import Card from '../../components/card/card';
+import Comment from './components/comment/comment';
 import {filterOffers, getCoords} from '../../store/actions';
 
 const Offer = (props) => {
@@ -121,52 +122,7 @@ const Offer = (props) => {
               <ul className="reviews__list">
                 {feedbacks === undefined ? null : feedbacks.map((item, index) => <Feedback key={index} feedback={item} />)}
               </ul>
-              <form className="reviews__form form" action="#" method="post">
-                <label className="reviews__label form__label" htmlFor="review">Your review</label>
-                <div className="reviews__rating-form form__rating">
-                  <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-                  <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                    <svg className="form__star-image" width="37" height="33">
-                      <use xlinkHref="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-                  <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                    <svg className="form__star-image" width="37" height="33">
-                      <use xlinkHref="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-                  <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                    <svg className="form__star-image" width="37" height="33">
-                      <use xlinkHref="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-                  <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                    <svg className="form__star-image" width="37" height="33">
-                      <use xlinkHref="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-                  <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                    <svg className="form__star-image" width="37" height="33">
-                      <use xlinkHref="#icon-star"></use>
-                    </svg>
-                  </label>
-                </div>
-                <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
-                <div className="reviews__button-wrapper">
-                  <p className="reviews__help">
-                    To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-                  </p>
-                  <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
-                </div>
-              </form>
+              {props.isAuthorized ? <Comment /> : ``}
             </section>
           </div>
         </div>
@@ -206,7 +162,8 @@ Offer.propTypes = {
   match: PropTypes.object,
   feedbacks: PropTypes.array.isRequired,
   onOfferClick: PropTypes.func.isRequired,
-  currentCity: PropTypes.string.isRequired
+  currentCity: PropTypes.string.isRequired,
+  isAuthorized: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = (state, ownProps) => Object.assign({}, ownProps, {
@@ -215,6 +172,7 @@ const mapStateToProps = (state, ownProps) => Object.assign({}, ownProps, {
   feedbacks: state.feedbacks,
   onOfferClick: state.onOfferClick,
   currentCity: state.currentCity,
+  isAuthorized: state.isAuthorized,
 });
 
 const mapDispatchToProps = {
